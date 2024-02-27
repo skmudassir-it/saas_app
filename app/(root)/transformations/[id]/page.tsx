c6225b65ee4fa345f2e7a9ff8d3a135b8902298f
skmudassir-it <96885904+skmudassir-it@users.noreply.github.com>
2024-02-27 00:43:17 +0000

    auth-mod

--- app/(root)/transformations/[id]/page.tsx
+++ app/(root)/transformations/[id]/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const TransformationsPage = () => {
     return (
-        <div >TransformationsPage</div>
+        <div >Transformations Page</div>
     )
 }
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,9 @@ import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
 
-const IBMPlex = IBM_Plex_Sans({ 
+const IBMPlex = IBM_Plex_Sans({
   subsets: ["latin"],
   weight: ['400', '500', '600', '700'],
   variable: '--font-ibm-plex'
@@ -20,10 +21,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={cn("font-IBMPlex antialiased, IBMPlex.variable")}>
-        {children}
+    <ClerkProvider appearance={{
+      variables: {colorPrimary: '#624cf5'}
+    }}>
+      <html lang="en">
+        <body className={cn("font-IBMPlex antialiased, IBMPlex.variable")}>
+          {children}
         </body>
-    </html>
+      </html>
+    </ClerkProvider>
   );
 }
